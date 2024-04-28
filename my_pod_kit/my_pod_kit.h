dc03e98f262761ca0a080b9977a7839d97dd00b2
SwiftMan <zheng.qi.hui@qq.com>
2024-04-28 09:14:56 +0800

    init

---
+++ my_pod_kit/my_pod_kit.h
@@ -0,0 +1,18 @@
+//
+//  my_pod_kit.h
+//  my_pod_kit
+//
+//  Created by 郑琪辉 on 2024/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for my_pod_kit.
+FOUNDATION_EXPORT double my_pod_kitVersionNumber;
+
+//! Project version string for my_pod_kit.
+FOUNDATION_EXPORT const unsigned char my_pod_kitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <my_pod_kit/PublicHeader.h>
+
+
